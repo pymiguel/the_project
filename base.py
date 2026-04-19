7ba149ba476a98b9e65780d5a8d6d3a56b343a3a
Miguel M M Marques <mmiguelmmmofficial@gmail.com>
2026-04-19 13:23:22 -0300

    calculadora 2000 1.0

--- base.py
+++ base.py
@@ -1,1 +1,29 @@
-print("hello world")
+#some thoughts about what i learned
+calculo = []
+wanted = input("what do you want to calculate? ")
+
+d = wanted.split()
+
+print(d)
+
+for i in d:
+    if i in ['+', '-', '/', '*']:
+        calculo.append(i)
+    else:
+        calculo.append(int(i))
+
+
+print (calculo)
+
+expression = ' '.join(map(str, calculo))
+
+print(expression)
+
+result = eval(expression)
+
+print(f"the result of the expression {expression} is: {result}")
+
+
+
+
+
